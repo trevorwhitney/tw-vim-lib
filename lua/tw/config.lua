a9b2ab1aa3850bf53f0d9cbacc265678076a38bb
Trevor Whitney <trevorjwhitney@gmail.com>
2021-11-12 10:56:21 -0700

    add custom nvim tree mappings for opening and closing nodes

--- lua/tw/config.lua
+++ lua/tw/config.lua
@@ -94,6 +94,19 @@ function Config.setup()
       border = "single"
     }
   }
+
+  local tree_cb = require'nvim-tree.config'.nvim_tree_callback
+  local custom_mappings = {
+      { key = "h",                         cb = tree_cb("close_node") },
+      { key = {"<CR>", "o", "<2-LeftMouse>", "l"}, cb = tree_cb("edit") },
+  }
+  require('nvim-tree').setup {
+   view = {
+     mappings = {
+       list = custom_mappings,
+     }
+   }
+  }
 end
 
 return Config
