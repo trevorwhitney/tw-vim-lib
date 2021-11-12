938bb4d253ac6ccaae87042d4164c8a6ed1814b4
Trevor Whitney <trevorjwhitney@gmail.com>
2021-11-12 08:46:42 -0700

    comment out plugins not ready for yet

--- lua/tw/packer.lua
+++ lua/tw/packer.lua
@@ -3,33 +3,27 @@ local Packer = {}
 function Packer.install(use)
   require('packer').startup(function()
     use 'wbthomason/packer.nvim' -- Package manager
-    -- UI to select things (files, grep results, open buffers...)
-    use { 'nvim-telescope/telescope.nvim', requires = { 'nvim-lua/plenary.nvim' } }
-    -- use 'itchyny/lightline.vim' -- Fancier statusline
-    -- Add git related info in the signs columns and popups
-    use { 'lewis6991/gitsigns.nvim', requires = { 'nvim-lua/plenary.nvim' } }
-    -- Highlight, edit, and navigate code using a fast incremental parsing library
-    use 'nvim-treesitter/nvim-treesitter'
-    -- Additional textobjects for treesitter
-    use 'nvim-treesitter/nvim-treesitter-textobjects'
-    use 'L3MON4D3/LuaSnip' -- Snippets plugin
-    use 'trevorwhitney/tw-vim-lib'
+    use 'trevorwhitney/tw-vim-lib' -- all my vim customizations
+
     use 'AndrewRadev/bufferize.vim'
+    use 'LeafCage/yankround.vim'
+    use 'benmills/vimux'
+    use 'benmills/vimux-golang'
     use 'christoomey/vim-tmux-navigator'
     use 'coachshea/vim-textobj-markdown'
     use 'dense-analysis/ale'
     use 'easymotion/vim-easymotion'
+    use 'folke/which-key.nvim'
     use 'google/vim-jsonnet'
-    use {'junegunn/fzf', run = function() vim.fn['fzf#install']() end }
-    use {'junegunn/fzf.vim', run = function() vim.fn['fzf#install']() end }
     use 'junegunn/vader.vim'
     use 'jvirtanen/vim-hcl'
     use 'kana/vim-textobj-entire'
     use 'kana/vim-textobj-user'
-    use 'LeafCage/yankround.vim'
+    use 'kyazdani42/nvim-web-devicons'
     use 'machakann/vim-highlightedyank'
-    use 'mattn/emmet-vim'
     use {'mg979/vim-visual-multi', branch = 'master' }
+    use 'nvim-treesitter/nvim-treesitter' -- Highlight, edit, and navigate code using a fast incremental parsing library
+    use 'nvim-treesitter/nvim-treesitter-textobjects' -- Additional textobjects for treesitter
     use 'pedrohdz/vim-yaml-folds'
     use 'roxma/vim-tmux-clipboard'
     use 'tommcdo/vim-exchange'
@@ -44,41 +38,52 @@ function Packer.install(use)
     use 'tpope/vim-rsi'
     use 'tpope/vim-surround'
     use 'tpope/vim-unimpaired'
+
+    -- replace airline with lightline?
     use 'vim-airline/vim-airline'
     use 'vim-airline/vim-airline-themes'
-    use {'yuki-yano/fzf-preview.vim', branch = 'release/rpc' }
-    use 'antoinemadec/coc-fzf'
-    use 'benmills/vimux'
-    use 'benmills/vimux-golang'
-    use { 'fannheyward/coc-markdownlint', run = 'yarn install --frozen-lockfile' }
-    use { 'fannheyward/coc-sql', run = 'yarn install --frozen-lockfile' }
-    use 'folke/which-key.nvim'
-    use 'kyazdani42/nvim-web-devicons'
+    -- use 'itchyny/lightline.vim' -- Fancier statusline
 
     -- begin coc
+    use {'neoclide/coc-highlight', run = 'yarn install --frozen-lockfile'}
+    use {'neoclide/coc-pairs', branch = 'master', run = 'yarn install --frozen-lockfile' }
+    use { 'weirongxu/coc-explorer', branch = 'master', run = 'yarn install --frozen-lockfile'}
+     --- end coc
+
+     -- will be repaced by telescope
+    use {'junegunn/fzf', run = function() vim.fn['fzf#install']() end }
+    use {'junegunn/fzf.vim', run = function() vim.fn['fzf#install']() end }
+    use {'yuki-yano/fzf-preview.vim', branch = 'release/rpc' }
+    -- UI to select things (files, grep results, open buffers...)
+    -- use { 'nvim-telescope/telescope.nvim', requires = { 'nvim-lua/plenary.nvim' } }
+
+    -- coc-git will be replaced by gitsigns
+    use {'neoclide/coc-git', run = 'yarn install --frozen-lockfile'}
+    -- Add git related info in the signs columns and popups
+    -- use { 'lewis6991/gitsigns.nvim', requires = { 'nvim-lua/plenary.nvim' } }
+
+    -- native lsp will replace coc
+    use {'neoclide/coc.nvim',branch = 'master', run = 'yarn install --frozen-lockfile' }
+    use {'neoclide/coc-yaml', run = 'yarn install --frozen-lockfile'}
+    use {'neoclide/coc-json', run = 'yarn install --frozen-lockfile'}
+    use {'neoclide/coc-tsserver', run = 'yarn install --frozen-lockfile'}
     use {'iamcco/coc-spell-checker', run = 'yarn install --frozen-lockfile'}
     use {'iamcco/coc-vimlsp', run = 'yarn install --frozen-lockfile'}
     use {'josa42/coc-lua', run = 'yarn install --frozen-lockfile'}
     use {'josa42/coc-go', run = 'yarn install --frozen-lockfile'}
     use {'josa42/coc-sh', run = 'yarn install --frozen-lockfile'}
-    use {'neoclide/coc-git', run = 'yarn install --frozen-lockfile'}
-    use {'neoclide/coc-highlight', run = 'yarn install --frozen-lockfile'}
-    use {'neoclide/coc-json', run = 'yarn install --frozen-lockfile'}
-    use {'neoclide/coc-snippets', run = 'yarn install --frozen-lockfile'}
-    use {'neoclide/coc-tsserver', run = 'yarn install --frozen-lockfile'}
-    use {'neoclide/coc-yaml', run = 'yarn install --frozen-lockfile'}
-    use {'neoclide/coc-yank', run = 'yarn install --frozen-lockfile'}
-    use {'neoclide/coc.nvim',branch = 'master', run = 'yarn install --frozen-lockfile' }
-    use {'neoclide/coc-pairs', branch = 'master', run = 'yarn install --frozen-lockfile' }
-    use { 'weirongxu/coc-explorer', branch = 'master', run = 'yarn install --frozen-lockfile'}
-     --- end coc
-
-    -- native lsp
+    use { 'fannheyward/coc-markdownlint', run = 'yarn install --frozen-lockfile' }
+    use { 'fannheyward/coc-sql', run = 'yarn install --frozen-lockfile' }
+    use 'antoinemadec/coc-fzf'
     -- use 'neovim/nvim-lspconfig' -- Collection of configurations for built-in LSP client
     -- use 'hrsh7th/nvim-cmp' -- Autocompletion plugin
     -- use 'hrsh7th/cmp-nvim-lsp'
-    -- use 'saadparwaiz1/cmp_luasnip'
     -- end native lsp
+
+    -- coc-snippets will be replaced by lua snip
+    use {'neoclide/coc-snippets', run = 'yarn install --frozen-lockfile'}
+    -- use 'L3MON4D3/LuaSnip' -- Snippets plugin
+    -- use 'saadparwaiz1/cmp_luasnip'
 
     use 'sebdah/vim-delve'
     use 'shaunsingh/solarized.nvim'
