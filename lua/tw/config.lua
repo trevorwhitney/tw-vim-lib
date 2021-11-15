289307c7c5f0bd958dd5bc09d645b291b898f1a8
Trevor Whitney <trevorjwhitney@gmail.com>
2021-11-14 21:06:12 -0700

    got snippets working

--- lua/tw/config.lua
+++ lua/tw/config.lua
@@ -121,6 +121,11 @@ local function configureFzf()
 end
 
 local function configureCmp()
+	local has_words_before = function()
+		local line, col = unpack(vim.api.nvim_win_get_cursor(0))
+		return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match("%s") == nil
+	end
+
 	local luasnip = require("luasnip")
 	local cmp = require("cmp")
 	cmp.setup({
@@ -140,16 +145,18 @@ local function configureCmp()
 				behavior = cmp.ConfirmBehavior.Replace,
 				select = true,
 			}),
-			["<Tab>"] = function(fallback)
+			["<Tab>"] = cmp.mapping(function(fallback)
 				if cmp.visible() then
 					cmp.select_next_item()
 				elseif luasnip.expand_or_jumpable() then
 					luasnip.expand_or_jump()
+				elseif has_words_before() then
+					cmp.complete()
 				else
 					fallback()
 				end
-			end,
-			["<S-Tab>"] = function(fallback)
+			end, {"i", "s"}),
+			["<S-Tab>"] = cmp.mapping(function(fallback)
 				if cmp.visible() then
 					cmp.select_prev_item()
 				elseif luasnip.jumpable(-1) then
@@ -157,13 +164,22 @@ local function configureCmp()
 				else
 					fallback()
 				end
-			end,
+			end, {"i", "s"})
 		},
 		sources = cmp.config.sources({
 			{ name = "nvim_lsp" },
 			{ name = "luasnip" },
+			{ name = "buffer" },
+			{ name = "path" },
 		}),
 	})
+
+	luasnip.config.set_config({
+		history = true,
+		updateevents = "TextChanged,TextChangedI",
+	})
+
+	require("luasnip/loaders/from_vscode").lazy_load()
 end
 
 function Config.setup()
