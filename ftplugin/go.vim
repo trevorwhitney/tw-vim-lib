7c757e3f6f20b51ada1fcb7186f46110047af788
Trevor Whitney <trevorjwhitney@gmail.com>
2021-12-01 14:16:08 -0700

    add vim-go for tags and tests

--- ftplugin/go.vim
+++ ftplugin/go.vim
@@ -1,18 +1,12 @@
-if g:use_native_lsp == 1
-  "TODO: are there any specific go stuff?
-  "impl at cursor and toggle test are important
-else
-  " open test in a vertical split
-  nmap <leader>gt  :<C-u>CocCommand go.test.toggle<cr>
-  nmap <silent>gT :<C-u>wincmd o<cr> :vsplit<cr> :<C-u>CocCommand go.test.toggle<cr>
-  nmap <leader>t   :<C-u>CocCommand go.test.generate.function<cr>
-  nmap <leader>i   :<C-u>CocCommand go.impl.cursor<cr>
+" open test in a vertical split
+nmap <leader>gt  :<C-u>GoAlternate<cr>
+nmap <silent>gT  :<C-u>wincmd o<cr> :vsplit<cr> :<C-u>GoAlternate<cr>
+nmap <leader>i   :<C-u>GoImpl<cr>
 
-  " tags
-  nmap <leader>tj :CocCommand go.tags.add json<cr>
-  nmap <leader>ty :CocCommand go.tags.add yaml<cr>
-  nmap <leader>tx :CocCommand go.tags.clear<cr>
-endif
+" tags
+nmap <leader>tj :GoAddTags json<cr>
+nmap <leader>ty :GoAddTags yaml<cr>
+nmap <leader>tx :GoRemoveTags<cr>
 
 " run tests
 nmap <Leader>rp  :wa<CR> :GolangTestCurrentPackage<CR>
@@ -27,6 +21,3 @@ nmap <leader>dt  :wa<cr> :DlvTestFocused<cr>
 
 " delve integration test
 nmap <leader>di  :wa<cr> :DlvTestFocused e2e_gme requires_docker<cr>
-
-" search
-nmap <leader>gr :Rg -g '**/*.go' --
--- lua/tw/config/vim-options.lua
+++ lua/tw/config/vim-options.lua
@@ -13,17 +13,17 @@ set.undofile = true
 set.showmatch = true -- Show matching brackets/braces
 set.smarttab = true -- Use shiftwidth to tab at line beginning
 
-set.backspace = {'indent', 'eol', 'start'}  -- Let backspace work over anything.
+set.backspace = { "indent", "eol", "start" } -- Let backspace work over anything.
 set.ignorecase = true -- ignore case only when search term is all lowercase
 set.smartcase = true -- ignore case only when search term is all lowercase
-set.mouse = 'a' -- enable mouse in all modes
+set.mouse = "a" -- enable mouse in all modes
 set.scrolloff = 5
 set.shiftwidth = 2 -- Width of autoindent
-set.switchbuf = 'useopen'
+set.switchbuf = "useopen"
 set.tabstop = 2 -- Tab settings
-set.tags:prepend('./.git/tags')
-set.undodir = vim.env.HOME .. '/.vim/undodir'
-set.encoding = 'utf-8'
+set.tags:prepend("./.git/tags")
+set.undodir = vim.env.HOME .. "/.vim/undodir"
+set.encoding = "utf-8"
 
 -- TextEdit might fail if hidden is not set.
 set.hidden = true
@@ -40,47 +40,53 @@ set.cmdheight = 2
 set.updatetime = 300
 
 -- Don't pass messages to |ins-completion-menu|.
-set.shortmess:append('c')
+set.shortmess:append("c")
 
 -- Open diffs vertically
-set.diffopt = 'vertical'
-set.clipboard = 'unnamedplus'
+set.diffopt = "vertical"
+set.clipboard = "unnamedplus"
 
 -- folding
-set.foldmethod = 'syntax'   -- fold based on indent
-set.foldnestmax = 10      -- deepest fold is 10 levels
-set.foldenable = false       -- dont fold by default
-set.foldlevel = 1         -- this is just what i use
+set.foldmethod = "syntax" -- fold based on indent
+set.foldnestmax = 10 -- deepest fold is 10 levels
+set.foldenable = false -- dont fold by default
+set.foldlevel = 1 -- this is just what i use
 set.foldlevelstart = 99
-set.foldopen = 'insert'     -- open folds when inserted into
+set.foldopen = "insert" -- open folds when inserted into
 
 -- Auto completion
-set.completeopt = {'menuone','noselect'}
-set.wildignore:append {'*\\tmp\\*','*.swp','*.swo','*.zip','.git','.cabal-sandbox'}
-set.wildmode = {'longest','list','full'}
+set.completeopt = { "menuone", "noselect" }
+set.wildignore:append({ "*\\tmp\\*", "*.swp", "*.swo", "*.zip", ".git", ".cabal-sandbox" })
+set.wildmode = { "longest", "list", "full" }
 set.wildmenu = true
-set.completeopt:append 'longest'
+set.completeopt:append("longest")
 
 -- Directories
-set.directory = {'~/.vim-tmp', '~/tmp', '/var/tmp', '/tmp'}
-set.backupdir = {'~/.vim-tmp', '~/tmp', '/var/tmp', '/tmp'}
+set.directory = { "~/.vim-tmp", "~/tmp", "/var/tmp", "/tmp" }
+set.backupdir = { "~/.vim-tmp", "~/tmp", "/var/tmp", "/tmp" }
 
 -- Always show the signcolumn, otherwise it would shift the text each time
 -- diagnostics appear/become resolved.
 if vim.fn.has("patch-8.1.1564") then
-  -- Recently vim can merge signcolumn and number column into one
-  set.signcolumn = 'number'
+	-- Recently vim can merge signcolumn and number column into one
+	set.signcolumn = "number"
 else
-  set.signcolumn = 'auto:2'
+	set.signcolumn = "auto:2"
 end
 
 -- disable python2 provider
-vim.g['loaded_python_provider'] = 0
-vim.g['python3_host_prog'] = '/usr/local/bin/python'
+vim.g["loaded_python_provider"] = 0
+vim.g["python3_host_prog"] = "/usr/local/bin/python"
 
 -- diable perl provider
-vim.g['loaded_perl_provider'] = 0
+vim.g["loaded_perl_provider"] = 0
 
 -- disable default markdown textobj mappings
-vim.g['textobj_markdown_no_default_key_mappings'] = 1
+vim.g["textobj_markdown_no_default_key_mappings"] = 1
 
+-- don't use vim-go for any gopls or completion functionality
+vim.g["go_code_completion_enabled"] = 0
+vim.g["go_def_mapping_enabled"] = 0
+vim.g["go_build_tags"] = "e2e_gem,requires_docker"
+vim.g["go_textobj_enabled"] = 0
+vim.g["go_gopls_enabled"] = 0
