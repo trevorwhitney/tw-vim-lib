6f9e4569d764b120d19fe3b47f4b373f07f9a088
Trevor Whitney <trevorjwhitney@gmail.com>
2022-01-21 16:52:22 -0700

    Remove gitsigns from null-ls

--- lua/tw/config/null-ls.lua
+++ lua/tw/config/null-ls.lua
@@ -4,7 +4,6 @@ local home = vim.loop.os_getenv("HOME")
 require("null-ls").setup({
 	-- you must define at least one source for the plugin to work
 	sources = {
-		null_ls.builtins.code_actions.gitsigns,
 		null_ls.builtins.code_actions.shellcheck,
 		null_ls.builtins.code_actions.statix,
 		null_ls.builtins.diagnostics.golangci_lint,
