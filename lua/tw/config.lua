99099d2bc92708dc76679bbb9a69c9b223728bcf
Trevor Whitney <trevorjwhitney@gmail.com>
2022-05-06 12:19:09 -0600

    Conditionally enable support for LSPs

--- lua/tw/config.lua
+++ lua/tw/config.lua
@@ -10,7 +10,11 @@ function Config.setup(sumneko_root, nix_rocks_tree)
 	require("tw.config.completion")
 	require("tw.config.dap")
 
-	require("tw.config.lsp").setup(sumneko_root, nix_rocks_tree)
+
+  local lsp_support = vim.api.nvim_eval('get(s:, "lsp_support", 0)')
+  if lsp_support == 1 then
+    require("tw.config.lsp").setup(sumneko_root, nix_rocks_tree)
+  end
 end
 
 return Config
