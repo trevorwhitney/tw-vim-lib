fb699b75868e0c250515c4cfbb05897aa66ea0ee
Trevor Whitney <trevorjwhitney@gmail.com>
2022-08-09 16:31:37 -0600

    Fix [c and ]c keybindings

--- lua/tw/packer.lua
+++ lua/tw/packer.lua
@@ -25,8 +25,25 @@ local function installNativeLsp(use)
           end
 
           -- Navigation
-          map("n", "]c", "&diff ? ']c' : '<cmd>Gitsigns next_hunk<CR>'", { expr = true })
-          map("n", "[c", "&diff ? '[c' : '<cmd>Gitsigns prev_hunk<CR>'", { expr = true })
+          map("n", "]c", function()
+            if vim.wo.diff then
+              return "]c"
+            end
+            vim.schedule(function()
+              gs.next_hunk({preview = true})
+            end)
+            return "<Ignore>"
+          end, { expr = true })
+
+          map("n", "[c", function()
+            if vim.wo.diff then
+              return "[c"
+            end
+            vim.schedule(function()
+              gs.prev_hunk({preview = true})
+            end)
+            return "<Ignore>"
+          end, { expr = true })
 
           -- Actions
           map({ "n", "v" }, "<leader>hs", ":Gitsigns stage_hunk<CR>")
