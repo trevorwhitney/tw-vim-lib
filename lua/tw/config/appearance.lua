804a639bec6e2aa6d16bee93e8fe45023fffff7b
Trevor Whitney <trevorjwhitney@gmail.com>
2023-02-07 11:50:29 -0700

    now try dayfox

--- lua/tw/config/appearance.lua
+++ lua/tw/config/appearance.lua
@@ -15,13 +15,10 @@ function M.setup()
   vim.g.solarized_disable_background = true
 
   -- Load dawnfox colorscheme
-  vim.cmd("colorscheme dawnfox")
+  vim.cmd("colorscheme dayfox")
 
   -- Load solarized colorscheme
   -- require("solarized").set()
-  -- solarized sets the background to dark
-  -- so we need to set it to light again
-  -- vim.opt.background = "light"
 end
 
 return M
