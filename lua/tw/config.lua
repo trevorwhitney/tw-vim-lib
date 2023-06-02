6c1d82accdd1267442a3a71da907543de75b9012
Trevor Whitney <trevorjwhitney@gmail.com>
2023-06-02 09:30:20 -0600

    Consistent naming of config modules

--- lua/tw/config.lua
+++ lua/tw/config.lua
@@ -2,21 +2,21 @@ local Config = {}
 
 function Config.setup(lua_ls_root, nix_rocks_tree)
   -- Use CapitalCamelCase to avoid collisioins with global lua modules
-  require("tw.config.vim-options").setup()
-  require("tw.config.appearance").setup()
+  require("tw.config.VimOptions").setup()
+  require("tw.config.Appearance").setup()
   require("tw.config.WhichKey").setup()
-  require("tw.config.nvim-tree").setup()
-  require("tw.config.null-ls").setup()
+  require("tw.config.NvimTree").setup()
+  require("tw.config.NullLs").setup()
   require("tw.config.Telescope").setup()
   require("tw.config.Dap").setup()
-  require("tw.config.treesitter").setup()
-  require("tw.config.copilot").setup()
-  require("tw.config.nvim-cmp").setup()
-  require("tw.config.git").setup()
+  require("tw.config.Treesitter").setup()
+  require("tw.config.Copilot").setup()
+  require("tw.config.NvimCmp").setup()
+  require("tw.config.Git").setup()
 
   local lsp_support = vim.api.nvim_eval('get(s:, "lsp_support", 0)')
   if lsp_support == 1 then
-    require("tw.config.lsp").setup(lua_ls_root, nix_rocks_tree)
+    require("tw.config.Lsp").setup(lua_ls_root, nix_rocks_tree)
   end
 end
 
