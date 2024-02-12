499bc846a6187e42648101b59dc96b5203b83df8
Trevor Whitney <trevorjwhitney@gmail.com>
2024-02-12 11:00:52 -0700

    Browse uses open instead of xdg-open

--- lua/tw/config/Commands.lua
+++ lua/tw/config/Commands.lua
@@ -5,7 +5,7 @@ function M.setup()
 	-- TODO: this is broken on mac because it's hardcoded to xdg-open
 	api.nvim_create_user_command(
 		"Browse",
-		"silent exe '!xdg-open \"' . tw#util#UrlEscape(<q-args>) . '\"'",
+		"silent exe '!open \"' . tw#util#UrlEscape(<q-args>) . '\"'",
 		{ bang = true, nargs = 1 }
 	)
 
