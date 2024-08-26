7cba41710939d1a2ac5596322acc64b88675bebf
Trevor Whitney <trevorjwhitney@gmail.com>
2024-08-26 15:39:24 -0600

    fix some which-key mapping updates

--- lua/tw/config/Git.lua
+++ lua/tw/config/Git.lua
@@ -36,28 +36,28 @@ local function setup()
       end, { expr = true })
 
       local keymap = {
-        { "<leader>g",  buffer = 1,                                                                     group = "Git", nowait = false,                        remap = false },
-        { "<leader>gS", "<cmd>lua require('gitsigns').stage_buffer()<cr>",                              buffer = 1,    desc = "Stage Buffer",                 nowait = false, remap = false },
-        { "<leader>gU", "<cmd>lua require('gitsigns').reset_buffer_index()<cr>",                        buffer = 1,    desc = "Reset Buffer Index",           nowait = false, remap = false },
-        { "<leader>gW", "<cmd>Gwrite!<cr>",                                                             buffer = 1,    desc = "Git write",                    nowait = false, remap = false },
-        { "<leader>gX", "<cmd>lua require('gitsigns').reset_buffer()<cr>",                              buffer = 1,    desc = "Reset Buffer",                 nowait = false, remap = false },
-        { "<leader>gb", "<cmd>lua require('gitsigns').blame_line({ full = true })<cr>",                 buffer = 1,    desc = "Blame",                        nowait = false, remap = false },
-        { "<leader>gc", "<cmd>lua require('gitsigns').toggle_current_line_blame()<cr>",                 buffer = 1,    desc = "Toggle Current Line Blame",    nowait = false, remap = false },
-        { "<leader>gd", "<cmd>lua require('tw.config.Git').diffSplit(vim.fn.input('[Commit] > '))<cr>", buffer = 1,    desc = "Diff Split (Against Commit)",  nowait = false, remap = false },
-        { "<leader>gh", ":0Gclog!<cr>",                                                                 buffer = 1,    desc = "History",                      nowait = false, remap = false },
-        { "<leader>gk", "Git commit<cr>",                                                               buffer = 1,    desc = "Commit",                       nowait = false, remap = false },
-        { "<leader>gl", ":<C-u>Git log -n 50 --graph --decorate --oneline<cr>",                         buffer = 1,    desc = "Log",                          nowait = false, remap = false },
-        { "<leader>go", "<cmd>lua require('tw.config.Git').browseCurrentLine()<cr>",                    buffer = 1,    desc = "Open Current Line in Browser", nowait = false, remap = false },
-        { "<leader>gp", "<cmd>lua require('gitsigns').preview_hunk()<cr>",                              buffer = 1,    desc = "Preview Hunk",                 nowait = false, remap = false },
-        { "<leader>gs", "<cmd>lua require('gitsigns').stage_hunk()<cr>",                                buffer = 1,    desc = "Stage Hunk",                   nowait = false, remap = false },
-        { "<leader>gu", "<cmd>lua require('gitsigns').undo_stage_hunk()<cr>",                           buffer = 1,    desc = "Undo Stage Hunk",              nowait = false, remap = false },
-        { "<leader>gw", "<cmd>Gwrite<cr>",                                                              buffer = 1,    desc = "Git write",                    nowait = false, remap = false },
-        { "<leader>gx", "<cmd>lua require('gitsigns').reset_hunk()<cr>",                                buffer = 1,    desc = "Reset Hunk",                   nowait = false, remap = false },
+        { "<leader>g",  group = "Git",                                                                  nowait = false,                        remap = false },
+        { "<leader>gS", "<cmd>lua require('gitsigns').stage_buffer()<cr>",                              desc = "Stage Buffer",                 nowait = false, remap = false },
+        { "<leader>gU", "<cmd>lua require('gitsigns').reset_buffer_index()<cr>",                        desc = "Reset Buffer Index",           nowait = false, remap = false },
+        { "<leader>gW", "<cmd>Gwrite!<cr>",                                                             desc = "Git write",                    nowait = false, remap = false },
+        { "<leader>gX", "<cmd>lua require('gitsigns').reset_buffer()<cr>",                              desc = "Reset Buffer",                 nowait = false, remap = false },
+        { "<leader>gb", "<cmd>lua require('gitsigns').blame_line({ full = true })<cr>",                 desc = "Blame",                        nowait = false, remap = false },
+        { "<leader>gc", "<cmd>lua require('gitsigns').toggle_current_line_blame()<cr>",                 desc = "Toggle Current Line Blame",    nowait = false, remap = false },
+        { "<leader>gd", "<cmd>lua require('tw.config.Git').diffSplit(vim.fn.input('[Commit] > '))<cr>", desc = "Diff Split (Against Commit)",  nowait = false, remap = false },
+        { "<leader>gh", ":0Gclog!<cr>",                                                                 desc = "History",                      nowait = false, remap = false },
+        { "<leader>gk", "Git commit<cr>",                                                               desc = "Commit",                       nowait = false, remap = false },
+        { "<leader>gl", ":<C-u>Git log -n 50 --graph --decorate --oneline<cr>",                         desc = "Log",                          nowait = false, remap = false },
+        { "<leader>go", "<cmd>lua require('tw.config.Git').browseCurrentLine()<cr>",                    desc = "Open Current Line in Browser", nowait = false, remap = false },
+        { "<leader>gp", "<cmd>lua require('gitsigns').preview_hunk()<cr>",                              desc = "Preview Hunk",                 nowait = false, remap = false },
+        { "<leader>gs", "<cmd>lua require('gitsigns').stage_hunk()<cr>",                                desc = "Stage Hunk",                   nowait = false, remap = false },
+        { "<leader>gu", "<cmd>lua require('gitsigns').undo_stage_hunk()<cr>",                           desc = "Undo Stage Hunk",              nowait = false, remap = false },
+        { "<leader>gw", "<cmd>Gwrite<cr>",                                                              desc = "Git write",                    nowait = false, remap = false },
+        { "<leader>gx", "<cmd>lua require('gitsigns').reset_hunk()<cr>",                                desc = "Reset Hunk",                   nowait = false, remap = false },
 
-        { "ih",         "<cmd>lua require('gitsigns').select_hunk()<cr>",                               buffer = 1,    desc = "Select In Hunk",               mode = "o",     nowait = false, remap = false },
+        { "ih",         "<cmd>lua require('gitsigns').select_hunk()<cr>",                               desc = "Select In Hunk",               mode = "o",     nowait = false, remap = false },
 
-        { "go",         ":'<,'>GBrowse<cr>",                                                            buffer = 1,    desc = "Open in Browser",              mode = "x",     nowait = false, remap = false },
-        { "ih",         "<cmd>lua require('gitsigns').select_hunk()<cr>",                               buffer = 1,    desc = "Select In Hunk",               mode = "x",     nowait = false, remap = false },
+        { "go",         ":'<,'>GBrowse<cr>",                                                            desc = "Open in Browser",              mode = "x",     nowait = false, remap = false },
+        { "ih",         "<cmd>lua require('gitsigns').select_hunk()<cr>",                               desc = "Select In Hunk",               mode = "x",     nowait = false, remap = false },
       }
 
       local whichkey = require("which-key")
