3fe691e75b4d44296d0fb770053de35b5c7cbe9a
Trevor Whitney <trevorjwhitney@gmail.com>
2025-01-17 16:57:58 -0700

    switch to nixpkgs rather than nixos

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "Neovim configured just how I like it";
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
     flake-utils.url = "github:numtide/flake-utils";
   };
 
