43c5cc93f694b53f6dd16cfec68c80957de4b8a1
Trevor Whitney <trevorjwhitney@gmail.com>
2025-06-10 09:19:21 -0600

    remove deprecated dressing plugin

--- lua/tw/packer.lua
+++ lua/tw/packer.lua
@@ -186,7 +186,6 @@ function Packer.install(use)
       "kyazdani42/nvim-tree.lua",
       requires = {
         "kyazdani42/nvim-web-devicons",
-        'stevearc/dressing.nvim"',
       },
     })
     use({
